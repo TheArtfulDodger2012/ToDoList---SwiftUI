d3f4e37d70c885b72bd368c0792a84a55da9eb10
Ron Lane <theartfuldodger2012@gmail.com>
2024-04-04 19:30:37 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ron Lane on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
