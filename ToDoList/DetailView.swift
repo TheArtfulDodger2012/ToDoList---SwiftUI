6daab4dda2bf7894da76555cf62fd32e19ff8a79
Ron Lane <theartfuldodger2012@gmail.com>
2024-04-04 21:54:28 -0400

    ToDo List Commit #1 - Experiment with NavigationStack/Link

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Ron Lane on 4/4/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ ToDoList/ToDoListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Ron Lane on 4/4/24.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        VStack {
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
